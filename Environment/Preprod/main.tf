3b1bba15aac31e543fb607920ac5c264e384be86
Sumit Chauhan <sumitchauhan797@gmail.com>
2026-08-14 05:21:56 +0530

    change variable name

--- Environment/Preprod/main.tf
+++ Environment/Preprod/main.tf
@@ -1,4 +1,4 @@
 module "resource_groups" {
-  source = "../../modules/resource_group"
-  resource_groups  = var.resource_groups
+  source          = "../../modules/resource_group"
+  resource_groups = var.resource_groups
 }
